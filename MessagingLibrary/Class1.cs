19e5d405fe44af178ffa8603140c811a97c5fef7
YAJATapps <yajatcomputer@gmail.com>
2022-09-20 15:41:56 -0700

    Get the message json string from server url

--- MessagingLibrary/Class1.cs
+++ MessagingLibrary/Class1.cs
@@ -1,10 +1,23 @@
-﻿namespace MessagingLibrary
+﻿using System.Net;
+
+namespace MessagingLibrary
 {
     public class Class1
     {
-        public string message()
+        // TODO: Replace with environment variable for server url. Temperarily testing with localhost
+        private static string ApiUrl = "https://localhost:7047";
+
+        public static string GetMessage()
         {
-            return "Hello from library";
+            string CallUrl = ApiUrl + "/MessageData";
+            string json;
+
+            using (WebClient wc = new WebClient())
+            {
+                json = wc.DownloadString(CallUrl);
+            }
+
+            return json;
         }
     }
 }
