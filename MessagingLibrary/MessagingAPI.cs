fc35ebb47d79ba63137f143b64e384d8ddef1259
YAJATapps <yajatcomputer@gmail.com>
2022-09-22 15:58:31 -0700

    Fix exception if url connection fails

---
+++ MessagingLibrary/MessagingAPI.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+
+namespace MessagingLibrary
+{
+    public class MessagingAPI
+    {
+        // TODO: Replace with environment variable for server url. Temperarily testing with localhost
+        private static string ApiUrl = "https://localhost:7047";
+
+        public static string GetMessages()
+        {
+            string CallUrl = ApiUrl + "/api/getMessages";
+            string json;
+
+            try
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    json = wc.DownloadString(CallUrl);
+                }
+            } catch (Exception e)
+            {
+                return "Error";
+            }
+
+            return json;
+        }
+    }
+}
