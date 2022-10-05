c77a7d8f0b8c9fe7f64d4b9265cdba7a3a4ba399
Yajat Kumar <yajatapps3@gmail.com>
2022-10-04 17:56:45 -0700

    Add method to get messages sent to a particular user

--- MessagingLibrary/MessagingAPI.cs
+++ MessagingLibrary/MessagingAPI.cs
@@ -7,6 +7,7 @@ namespace MessagingLibrary
 
         private static string ApiUrl = Environment.GetEnvironmentVariable("SERVER_API_URL");
 
+        // Get all the messages sent to all users from server
         public static string GetMessages()
         {
             string CallUrl = ApiUrl + "/api/getMessages";
@@ -27,6 +28,7 @@ namespace MessagingLibrary
             return Json;
         }
 
+        // Send a message to a user from server
         public static async Task<string> SendMessage(String SentTime, String Content, String MessageCategory, String MessageUser)
         {
             string CallUrl = ApiUrl + "/api/sendMessage";
@@ -46,6 +48,28 @@ namespace MessagingLibrary
             }
 
             return responseString;
+        }
+
+        // Get the messages sent to a user from server
+        public static string GetMessagesForUser(String User)
+        {
+            string CallUrl = ApiUrl + "/api/getMessagesForUser";
+            string Parameters = "?User=" + User;
+            string Json;
+
+            try
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    Json = wc.DownloadString(CallUrl + Parameters);
+                }
+            }
+            catch (Exception e)
+            {
+                return "Error";
+            }
+
+            return Json;
         }
     }
 }
