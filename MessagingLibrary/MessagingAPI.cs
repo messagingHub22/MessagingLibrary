c16ad180c05ea9a149a4bea23f3e2c241513783b
Yajat Kumar <yajatapps3@gmail.com>
2022-10-25 14:45:59 -0700

    Add methods to get groups, members and add member to group

--- MessagingLibrary/MessagingAPI.cs
+++ MessagingLibrary/MessagingAPI.cs
@@ -113,5 +113,84 @@ namespace MessagingLibrary
 
             return responseString;
         }
+
+        // Get the groups
+        public async static Task<List<string>> GetGroups()
+        {
+            string CallUrl = ApiUrl + "/api/getGroups";
+
+            string? responseString;
+            try
+            {
+                HttpClient client = new HttpClient();
+
+                var response = await client.GetAsync(CallUrl);
+                responseString = await response.Content.ReadAsStringAsync();
+
+                var model = JsonConvert.DeserializeObject<List<string>>(responseString);
+
+                if (model == null)
+                {
+                    throw new Exception("Empty list"); // Returns new empty list
+                }
+
+                return model;
+            }
+            catch (Exception e)
+            {
+                return new List<String>();
+            }
+        }
+
+        // Add a member to a group
+        public static async Task<string> AddMemberToGroup(String GroupName, String MemberName)
+        {
+            string CallUrl = ApiUrl + "/api/addMemberToGroup";
+            string Parameters = "?GroupName=" + GroupName + "&MemberName=" + MemberName;
+
+            string? responseString;
+            try
+            {
+                HttpClient client = new HttpClient();
+
+                var response = await client.PostAsync(CallUrl + Parameters, null);
+                responseString = await response.Content.ReadAsStringAsync();
+            }
+            catch (Exception e)
+            {
+                responseString = "Error";
+            }
+
+            return responseString;
+        }
+
+        // Get the members in a group
+        public async static Task<List<string>> GetGroupMembers(String Group)
+        {
+            string CallUrl = ApiUrl + "/api/getGroupMembers";
+            string Parameters = "?Group=" + Group;
+
+            string? responseString;
+            try
+            {
+                HttpClient client = new HttpClient();
+
+                var response = await client.GetAsync(CallUrl + Parameters);
+                responseString = await response.Content.ReadAsStringAsync();
+
+                var model = JsonConvert.DeserializeObject<List<string>>(responseString);
+
+                if (model == null)
+                {
+                    throw new Exception("Empty list"); // Returns new empty list
+                }
+
+                return model;
+            }
+            catch (Exception e)
+            {
+                return new List<string>();
+            }
+        }
     }
 }
