2ec67130b39366c3f151e28c364ef8cc6d83aa94
Yajat Kumar <yajatapps3@gmail.com>
2022-10-28 15:56:24 -0700

    Remove constant ping update method

--- MessagingLibrary/HubLink.cs
+++ MessagingLibrary/HubLink.cs
@@ -7,34 +7,16 @@ namespace MessagingLibrary
     public class HubLink
     {
 
-        // Last updated times for every user
-        private static Dictionary<string, DateTime> ReloadTimes = new Dictionary<string, DateTime>();
-
         // The signalR hub connection
         private static HubConnection? connection;
 
-
-        // Mark the message with the given Id as read
-        public static async void LoginUser(String User)
+        // Try to connect to hub
+        public static async void StartConnection()
         {
-            if (ReloadTimes.ContainsKey(User))
-            {
-                ReloadTimes[User] = DateTime.Now;
-            }
-            else
-            {
-                ReloadTimes.Add(User, DateTime.MinValue);
-            }
-
             connection = new HubConnectionBuilder()
                 .WithUrl(new Uri(MessagingAPI.ApiUrl + "/messagingHub"))
                 .WithAutomaticReconnect()
                 .Build();
-
-            connection.On<string>("ReloadMessageClient", (userClient) =>
-            {
-                ReloadTimes[userClient] = DateTime.Now;
-            });
 
             try
             {
@@ -49,6 +31,11 @@ namespace MessagingLibrary
         // Send message to all connected signalR clients
         public static async void SendReloadMessage(String User)
         {
+            if (connection == null)
+            {
+                await Task.Run(() => StartConnection());
+            }
+
             try
             {
                 await connection.InvokeAsync("ReloadMessage", User);
@@ -58,10 +45,5 @@ namespace MessagingLibrary
             }
         }
 
-        // The updated time for last message for this user
-        public static string ChangedTime(String User)
-        {
-            return ReloadTimes[User].ToString();
-        }
     }
 }
